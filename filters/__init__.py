4c26647057e56ba12f5b67334d0ec7286c181aa2
Ulug'bek <119843506+ulugbekhusainovv@users.noreply.github.com>
2024-03-11 12:24:57 +0500

    last commit

--- filters/__init__.py
+++ filters/__init__.py
@@ -3,7 +3,7 @@ from .private_chat import IsPrivate
 from .group_admins import IsGroupAdmin
 from .hiddenLink import ContainsHiddenLink
 from .bad_words import CheckBadWords
-from .video import IsVideo
+# from .video import IsVideo
 from .is_admin import IsAdmin
 from .is_bot_admin import IsBotAdmin
 from .is_bot_hasdelete import IsBotDelete
